4004ce1dc172fbdebc7bfab467bd83e31f2ff012
Anthony McKernan <anthonymckernan@gmail.com>
2019-07-01 14:34:50 +0100

    pr fixes

--- app/helpers/conviction_helper.rb
+++ app/helpers/conviction_helper.rb
@@ -3,9 +3,7 @@ module ConvictionHelper
     return t('conviction_title.never', scope: 'results/conviction') unless object.instance_of?(Date)
 
     statement_type = object < Date.today ? 'past' : 'present'
-    statement = t("conviction_title.#{statement_type}", scope: 'results/conviction')
-
-    "#{statement} #{I18n.l(object)}"
+    t("conviction_title.#{statement_type}", scope: 'results/conviction', date: I18n.l(object))
   end
 
   def conviction_statement_string(object)
