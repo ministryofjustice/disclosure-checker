5b0d798cc782972aeafd6d41d23de6faea83dff3
Anthony McKernan <anthonymckernan@gmail.com>
2019-10-21 15:55:33 +0100

    Add check your answer presnter

--- app/controllers/steps/check/check_your_answers_controller.rb
+++ app/controllers/steps/check/check_your_answers_controller.rb
@@ -2,11 +2,7 @@ module Steps
   module Check
     class CheckYourAnswersController < Steps::CheckStepController
       def show
-        @presenters = current_disclosure_report.check_groups.map do |check_group|
-          check_group.disclosure_checks.map do |disclosure_check|
-            ResultsPresenter.build(disclosure_check)
-          end
-        end.flatten
+        @presenters = CheckAnswersPresenter.new(current_disclosure_report)
       end
     end
   end
