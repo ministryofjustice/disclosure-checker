8a5c2492ce489b3fa35d8f37ddc93327e971b33d
Anthony McKernan <anthonymckernan@gmail.com>
2019-10-22 08:02:20 +0100

    remove check row partial

--- app/controllers/steps/check/check_your_answers_controller.rb
+++ app/controllers/steps/check/check_your_answers_controller.rb
@@ -2,7 +2,7 @@ module Steps
   module Check
     class CheckYourAnswersController < Steps::CheckStepController
       def show
-        @presenters = CheckAnswersPresenter.new(current_disclosure_report)
+        @presenter = CheckAnswersPresenter.new(current_disclosure_report)
       end
     end
   end
