d704809b4fe9c5d846ecc14cc761ef6dbec5f179
David Silva <Davidslv@users.noreply.github.com>
2021-04-28 10:41:11 +0100

    Remove reference to non endorsed FPN
    Back in PR #466 we have agreed to only have endorsed FPN's in the 
    service - this is due to non endorsed FPN not being considered a 
    conviction. 
    
    Now that we have the possibility to calculate multiple convictions, we 
    no longer support non endorsed FPN's as they are not a conviction.
    
    This is more of a follow up cleanup on #466

--- spec/services/calculators/motoring/adult/penalty_notice_spec.rb
+++ spec/services/calculators/motoring/adult/penalty_notice_spec.rb
@@ -6,20 +6,12 @@ RSpec.describe Calculators::Motoring::Adult::PenaltyNotice do
   let(:disclosure_check) { build(:disclosure_check,
                                  under_age: under_age,
                                  known_date: known_date,
-                                 motoring_endorsement: motoring_endorsement) }
+                                 motoring_endorsement: GenericYesNo::YES) }
 
   let(:under_age) { GenericYesNo::NO }
   let(:known_date) { Date.new(2018, 10, 31) }
-  let(:motoring_endorsement) { GenericYesNo::NO }
 
   describe '#expiry_date' do
-    context 'with a motoring endorsement' do
-      let(:motoring_endorsement) { GenericYesNo::YES }
-      it { expect(subject.expiry_date.to_s).to eq('2023-10-31') }
-    end
-
-    context 'without a motoring endorsement' do
-      it { expect(subject.expiry_date).to eq(:no_record) }
-    end
+    it { expect(subject.expiry_date.to_s).to eq('2023-10-31') }
   end
 end
--- spec/services/calculators/motoring/youth/penalty_notice_spec.rb
+++ spec/services/calculators/motoring/youth/penalty_notice_spec.rb
@@ -6,21 +6,12 @@ RSpec.describe Calculators::Motoring::Youth::PenaltyNotice do
   let(:disclosure_check) { build(:disclosure_check,
                                  under_age: under_age,
                                  known_date: known_date,
-                                 motoring_endorsement: motoring_endorsement) }
+                                 motoring_endorsement: GenericYesNo::YES) }
 
   let(:under_age) { GenericYesNo::YES }
   let(:known_date) { Date.new(2020, 1, 1) }
-  let(:motoring_endorsement) { GenericYesNo::NO }
 
   describe '#expiry_date' do
-    context 'with a motoring endorsement' do
-      let(:motoring_endorsement) { GenericYesNo::YES }
-
-      it { expect(subject.expiry_date.to_s).to eq('2022-07-01') }
-    end
-
-    context 'without a motoring endorsement' do
-      it { expect(subject.expiry_date).to eq(:no_record) }
-    end
+    it { expect(subject.expiry_date.to_s).to eq('2022-07-01') }
   end
 end
