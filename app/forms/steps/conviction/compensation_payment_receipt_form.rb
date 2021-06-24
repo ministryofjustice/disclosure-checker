003941b59d86d709939b6ee8d0adfa1cc021305f
Jesus Laiz <zheileman@users.noreply.github.com>
2021-06-24 11:35:02 +0100

    Cleanup compensation receipt kick out leftovers
    Follow-up to PR #533 to remove the controllers, form objects, views, etc. of the now removed receipt kick out.
    
    In a separate PR there will be some more cleaning, including DB attributes.

--- app/forms/steps/conviction/compensation_payment_receipt_form.rb
+++
@@ -1,9 +0,0 @@
-module Steps
-  module Conviction
-    class CompensationPaymentReceiptForm < BaseForm
-      include SingleQuestionForm
-
-      yes_no_attribute :compensation_receipt_sent
-    end
-  end
-end
