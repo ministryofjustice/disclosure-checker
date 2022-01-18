faf3da7b32d40e8f328ea47cc41d26f70cbd6b41
Jesus Laiz <zheileman@users.noreply.github.com>
2022-01-18 11:32:26 +0000

    Expose new analytics helper methods
    These will be used to know when to enable/disable the analytics JS code snippet.

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -9,6 +9,18 @@ module AnalyticsHelper
     ENV['GA_TRACKING_ID']
   end
 
+  def analytics_consent_cookie
+    cookies[Rails.configuration.x.cookies_consent_name]
+  end
+
+  def analytics_consent_accepted?
+    analytics_consent_cookie.eql?(CookieSettingsForm::CONSENT_ACCEPT)
+  end
+
+  def analytics_allowed?
+    analytics_tracking_id.present? && analytics_consent_accepted?
+  end
+
   def track_transaction(attributes)
     return unless current_disclosure_check.present?
 
