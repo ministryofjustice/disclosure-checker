0be790b79f326d306625f64e01de96b623abde2e
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2022-11-25 16:14:22 +0000

    Check disclosure_check is valid for the step

--- app/controllers/steps/caution_step_controller.rb
+++ app/controllers/steps/caution_step_controller.rb
@@ -1,6 +1,16 @@
 module Steps
   class CautionStepController < StepController
+    before_action :check_for_caution
+
     private
+
+    def check_for_caution
+      if current_disclosure_check.kind != "caution"
+        current_disclosure_check.navigation_stack.pop
+        current_disclosure_check.save
+        redirect_to root_path
+      end
+    end
 
     def decision_tree_class
       CautionDecisionTree
--- app/controllers/steps/conviction_step_controller.rb
+++ app/controllers/steps/conviction_step_controller.rb
@@ -1,6 +1,16 @@
 module Steps
   class ConvictionStepController < StepController
+    before_action :check_for_conviction
+
     private
+
+    def check_for_conviction
+      if current_disclosure_check.kind != "conviction"
+        current_disclosure_check.navigation_stack.pop
+        current_disclosure_check.save
+        redirect_to root_path
+      end
+    end
 
     def decision_tree_class
       ConvictionDecisionTree
