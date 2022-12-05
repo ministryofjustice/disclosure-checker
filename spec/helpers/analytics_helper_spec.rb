003b569f0885c7a0722f09f967c0394606382b23
digitalali-moj <javid.ali@digital.justice.gov.uk>
2022-12-05 12:14:25 +0000

    CDPT-218 Fix tests

--- spec/helpers/analytics_helper_spec.rb
+++ spec/helpers/analytics_helper_spec.rb
@@ -34,29 +34,19 @@ RSpec.describe AnalyticsHelper, type: :helper do
 
   describe '#analytics_allowed?' do
     before do
-      allow(ENV).to receive(:[]).with('GA_TRACKING_ID').and_return(tracking_id)
       allow(helper).to receive(:analytics_consent_accepted?).and_return(consent_accepted)
     end
 
-    let(:tracking_id) { nil }
     let(:consent_accepted) { nil }
 
-    context 'when there is no GA_TRACKING_ID set' do
-      it { expect(helper.analytics_allowed?).to eq(false) }
+    context 'and consent has been granted by the user' do
+      let(:consent_accepted) { true }
+      it { expect(helper.analytics_allowed?).to eq(true) }
     end
 
-    context 'when there is GA_TRACKING_ID set' do
-      let(:tracking_id) { 'xyz123' }
-
-      context 'and consent has been granted by the user' do
-        let(:consent_accepted) { true }
-        it { expect(helper.analytics_allowed?).to eq(true) }
-      end
-
-      context 'and consent has not been granted by the user' do
-        let(:consent_accepted) { false }
-        it { expect(helper.analytics_allowed?).to eq(false) }
-      end
+    context 'and consent has not been granted by the user' do
+      let(:consent_accepted) { false }
+      it { expect(helper.analytics_allowed?).to eq(false) }
     end
   end
 
