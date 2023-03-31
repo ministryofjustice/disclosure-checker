34047ce234c53ebd8e0f4737631264ec8ffd7d8c
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-03-31 11:08:15 +0100

    Disable sentry running if not production like environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,5 @@
-# Will use SENTRY_DSN environment variable if set
 Sentry.init do |config|
-  config.dsn = "https://d82ca719a5b246bf80342c2266fe7550@o345774.ingest.sentry.io/5373163"
+  config.dsn = Rails.application.config.sentry_dsn
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Ensure we get ActionView::MissingTemplate errors
