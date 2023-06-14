098b1bfee9af6a728d86f292420af89f9b0786c8
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-06-14 11:44:56 +0100

    Disable rubocop for old migrations

--- db/migrate/20210624131926_remove_compensation_amount_fields.rb
+++ db/migrate/20210624131926_remove_compensation_amount_fields.rb
@@ -1,6 +1,8 @@
 class RemoveCompensationAmountFields < ActiveRecord::Migration[6.1]
   def change
+    # rubocop:disable Rails/BulkChangeTable
     remove_column :disclosure_checks, :compensation_payment_over_100, :string
     remove_column :disclosure_checks, :compensation_receipt_sent, :string
+    # rubocop:enable Rails/BulkChangeTable
   end
 end
