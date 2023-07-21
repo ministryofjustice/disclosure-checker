a5258846b4505abf726f28f4b48659b3104f2ad8
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-07-21 11:52:37 +0100

    remove authentication tests

--- spec/controllers/backoffice/reports_controller_spec.rb
+++ spec/controllers/backoffice/reports_controller_spec.rb
@@ -1,10 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Backoffice::ReportsController, type: :controller do
-  before do
-    allow(controller).to receive(:check_http_credentials)
-  end
-
   it "responds with HTTP success" do
     get :index
     expect(response).to be_successful
@@ -20,14 +16,4 @@ RSpec.describe Backoffice::ReportsController, type: :controller do
     expect(response).to render_template(:index)
   end
 
-  context "when using credentials" do
-    before do
-      allow(controller).to receive(:check_http_credentials).and_call_original
-    end
-
-    it "responds with HTTP success" do
-      get :index
-      expect(response).to have_http_status(:unauthorized)
-    end
-  end
 end
