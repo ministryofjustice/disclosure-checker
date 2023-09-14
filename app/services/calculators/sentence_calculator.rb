d036ec948971dbfb7e36218d9a12255d42d2a427
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-09-14 16:27:23 +0100

    Create rehabilitation_period method per class

--- app/services/calculators/sentence_calculator.rb
+++ app/services/calculators/sentence_calculator.rb
@@ -3,50 +3,60 @@ module Calculators
     NEVER_SPENT_THRESHOLD = 48
     BAIL_OFFSET = -1.0
 
-    # If conviction length is 12 months or less: start date + length + 6 months
-    # If conviction length is over 12 months and less than or equal to 4 years: start date + length + 2 years
-    # If conviction length is over 4 years: start date + length + 3.5 years
-    #
     class Detention < SentenceCalculator
       UPPER_LIMIT = Float::INFINITY
 
-      REHABILITATION_1 = { months: 6 }.freeze
-      REHABILITATION_2 = { months: 24 }.freeze
-      REHABILITATION_3 = { months: 42 }.freeze
+      def rehabilitation_period
+        case conviction_length_in_months
+        when 0..12
+          { months: 6 }
+        when 13..48
+          { months: 24 }
+        else
+          { months: 42 }
+        end
+      end
     end
 
-    # If conviction length is 12 months or less: start date + length + 6 months
-    # If conviction length is over 12 months and less than or equal to 2 years: start date + length + 2 years
-    # If conviction length is over 2 years, it is considered invalid
-    #
     class DetentionTraining < SentenceCalculator
       UPPER_LIMIT = 24
 
-      REHABILITATION_1 = { months: 6 }.freeze
-      REHABILITATION_2 = { months: 24 }.freeze
+      def rehabilitation_period
+        case conviction_length_in_months
+        when 0..12
+          { months: 6 }
+        else
+          { months: 24 }
+        end
+      end
     end
 
-    # If conviction length is 12 months or less: start date + length + 1 year
-    # If conviction length is over 12 months and less than or equal to 4 years: start date + length + 4 years
-    # If conviction length is over 4 years: start date + length + 7 years
-    #
     class Prison < SentenceCalculator
       UPPER_LIMIT = Float::INFINITY
 
-      REHABILITATION_1 = { months: 12 }.freeze
-      REHABILITATION_2 = { months: 48 }.freeze
-      REHABILITATION_3 = { months: 84 }.freeze
+      def rehabilitation_period
+        case conviction_length_in_months
+        when 0..12
+          { months: 12 }
+        when 13..48
+          { months: 48 }
+        else
+          { months: 84 }
+        end
+      end
     end
 
-    # If conviction length is 12 months or less: start date + length + 1 year
-    # If conviction length is over 12 months and less than or equal to 2 years: start date + length + 4 years
-    # If conviction length is over 2 years, it is considered invalid
-    #
     class SuspendedPrison < SentenceCalculator
       UPPER_LIMIT = 24
 
-      REHABILITATION_1 = { months: 12 }.freeze
-      REHABILITATION_2 = { months: 48 }.freeze
+      def rehabilitation_period
+        case conviction_length_in_months
+        when 0..12
+          { months: 12 }
+        else
+          { months: 48 }
+        end
+      end
     end
 
     def expiry_date
@@ -63,17 +73,6 @@ module Calculators
     end
 
   private
-
-    def rehabilitation_period
-      case conviction_length_in_months
-      when 0..12
-        self.class::REHABILITATION_1
-      when 13..48
-        self.class::REHABILITATION_2
-      else
-        self.class::REHABILITATION_3
-      end
-    end
 
     # The day before the end date, thus we subtract 1 day.
     def conviction_end_date
