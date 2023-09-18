e28e91ecbc7c69c5105af9a604eda625be04cc14
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-09-18 14:45:50 +0100

    Revert "Add schedule_18_offence field"
    This reverts commit e6d999a5723dd73cecd24d2c752a2b80913bae42.

--- db/migrate/20230915162044_add_schedule_18_offence_to_disclosure_check.rb
+++
@@ -1,5 +0,0 @@
-class AddSchedule18OffenceToDisclosureCheck < ActiveRecord::Migration[6.1]
-  def change
-    add_column :disclosure_checks, :schedule_18_offence, :boolean, default: false
-  end
-end
