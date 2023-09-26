ceee345f8b3e11f99c25c554c2c840e4fe7ea6aa
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-09-26 15:35:25 +0100

    Add hint text

--- app/forms/steps/conviction/conviction_schedule18_form.rb
+++ app/forms/steps/conviction/conviction_schedule18_form.rb
@@ -4,6 +4,12 @@ module Steps
       include SingleQuestionForm
 
       yes_no_attribute :conviction_schedule18
+
+      # As we reuse this form object in multiple views, this is the attribute
+      # that will be used to choose the locales for legends and hints.
+      def i18n_attribute
+        conviction_subtype
+      end
     end
   end
 end
