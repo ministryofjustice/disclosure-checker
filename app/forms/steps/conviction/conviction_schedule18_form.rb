9bfe33bb3db325aa916e2913bd2ce18218b6aea7
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-09-27 08:08:35 +0100

    use static key and add test

--- app/forms/steps/conviction/conviction_schedule18_form.rb
+++ app/forms/steps/conviction/conviction_schedule18_form.rb
@@ -5,10 +5,8 @@ module Steps
 
       yes_no_attribute :conviction_schedule18
 
-      # As we reuse this form object in multiple views, this is the attribute
-      # that will be used to choose the locales for legends and hints.
       def i18n_attribute
-        conviction_subtype
+        "conviction_schedule18_text"
       end
     end
   end
