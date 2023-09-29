3d7f3e86a3f46755fc78f4a36eefac80013daf7c
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-09-29 12:16:33 +0100

    correct syntax error

--- spec/forms/steps/conviction/single_sentence_length_form_spec.rb
+++ spec/forms/steps/conviction/single_sentence_length_form_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe Steps::Conviction::SingleSentenceLengthForm do
   it_behaves_like "a yes-no question form", attribute_name: :single_sentence_length
