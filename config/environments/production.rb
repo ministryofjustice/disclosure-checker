9902df6cab44bc28509f688cb8b1dcddfc12efbd
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-15 12:44:35 +0100

    Allow govuk-frontend assets to be used

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Enable assets to be served from node_modules
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
