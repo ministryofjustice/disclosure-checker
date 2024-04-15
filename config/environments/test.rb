af1b5646f2a3d4c65cfea6d26715e17da8b4e03c
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-15 12:30:58 +0100

    Update test config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,12 +23,9 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
-
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = :none
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
