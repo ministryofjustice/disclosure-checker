8fac960c9b99a3385e088b6d8997d7c912d32799
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-09-16 18:28:11 +0100

    correct rubocop errors

--- app/value_objects/conviction_type.rb
+++ app/value_objects/conviction_type.rb
@@ -44,7 +44,7 @@ class ConvictionType < ValueObject
     # Youth convictions #
     #####################
 
-     REFERRAL_ORDER                     = new(:referral_order,                   parent: REFERRAL_SUPERVISION_YRO, relevant_order: true, no_drag_through: false, calculator_class: Calculators::AdditionCalculator::PlusZeroMonths),
+    REFERRAL_ORDER                     = new(:referral_order,                 parent: REFERRAL_SUPERVISION_YRO, relevant_order: true, no_drag_through: false, calculator_class: Calculators::AdditionCalculator::PlusZeroMonths),
     SUPERVISION_ORDER                  = new(:supervision_order,              parent: REFERRAL_SUPERVISION_YRO, no_drag_through: true, calculator_class: Calculators::AdditionCalculator::PlusZeroMonths),
     YOUTH_REHABILITATION_ORDER         = new(:youth_rehabilitation_order,     parent: REFERRAL_SUPERVISION_YRO, relevant_order: true, calculator_class: Calculators::AdditionCalculator::PlusZeroMonths),
     YOUTH_OTHER_REQUIREMENT_ORDER      = new(:youth_other_requirement_order,  parent: REFERRAL_SUPERVISION_YRO, relevant_order: true, no_drag_through: true, calculator_class: Calculators::AdditionCalculator::PlusZeroMonths),
@@ -104,14 +104,13 @@ class ConvictionType < ValueObject
     ADULT_REPRIMAND                      = new(:adult_reprimand,                      parent: ADULT_MILITARY, skip_length: true, calculator_class: Calculators::AdditionCalculator::StartPlusTwelveMonths),
 
     ADULT_DISQUALIFICATION               = new(:adult_disqualification,               parent: ADULT_MOTORING, relevant_order: true, no_drag_through: true, calculator_class: Calculators::DisqualificationCalculator::Adults),
-    ADULT_MOTORING_FINE                 = new(:adult_motoring_fine,                parent: ADULT_MOTORING, skip_length: true, no_drag_through: false, calculator_class: Calculators::Motoring::Adult::Fine),
-    ADULT_PENALTY_NOTICE                = new(:adult_penalty_notice,               parent: ADULT_MOTORING, skip_length: true, no_drag_through: true, calculator_class: Calculators::Motoring::Adult::PenaltyNotice),
+    ADULT_MOTORING_FINE                  = new(:adult_motoring_fine,                  parent: ADULT_MOTORING, skip_length: true, no_drag_through: false, calculator_class: Calculators::Motoring::Adult::Fine),
+    ADULT_PENALTY_NOTICE                 = new(:adult_penalty_notice,                 parent: ADULT_MOTORING, skip_length: true, no_drag_through: true, calculator_class: Calculators::Motoring::Adult::PenaltyNotice),
     ADULT_PENALTY_POINTS                 = new(:adult_penalty_points,                 parent: ADULT_MOTORING, skip_length: true, calculator_class: Calculators::Motoring::Adult::PenaltyPoints),
 
     ADULT_HOSPITAL_ORDER                 = new(:adult_hospital_order,                 parent: ADULT_CUSTODIAL_SENTENCE, relevant_order: true, calculator_class: Calculators::AdditionCalculator::PlusZeroMonths),
     ADULT_PRISON_SENTENCE                = new(:adult_prison_sentence,                parent: ADULT_CUSTODIAL_SENTENCE, calculator_class: Calculators::SentenceCalculator::Prison),
     ADULT_SUSPENDED_PRISON_SENTENCE      = new(:adult_suspended_prison_sentence,      parent: ADULT_CUSTODIAL_SENTENCE, calculator_class: Calculators::SentenceCalculator::SuspendedPrison),
-
   ].flatten.freeze
 
   # :nocov:
