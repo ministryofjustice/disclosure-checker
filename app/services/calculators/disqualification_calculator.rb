deac4c924957f983d5a0156b8c58c0b34b2eca4d
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-10-04 14:37:51 +0100

    remove code not needed

--- app/services/calculators/disqualification_calculator.rb
+++ app/services/calculators/disqualification_calculator.rb
@@ -1,14 +1,12 @@
 module Calculators
   class DisqualificationCalculator < BaseCalculator
-    ADDED_TIME = { months: 0 }.freeze
-
     def expiry_date
       return ResultsVariant::INDEFINITE if indefinite_length?
 
       if disclosure_check.conviction_length?
         conviction_end_date
       else
-        conviction_start_date.advance(ADDED_TIME)
+        conviction_start_date
       end
     end
   end
