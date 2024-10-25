9f5a68ef4afdb6499ee3e20602c5d2dff650054b
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-10-25 12:13:21 +0100

    Update gems and fix assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
+//= link images/favicon.ico
+//= link images/favicon.svg
+//= link images/govuk-icon-mask.svg
+//= link images/govuk-icon-180.png
+//= link images/govuk-crest.svg
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
