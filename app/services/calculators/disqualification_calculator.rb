ef2d54949d33aaa43b4ab98882280f7efe1321b5
N.Preddy <nick.preddy@digital.justice.gov.uk>
2025-02-06 14:15:07 +0000

    Add 6 months rehab when no end date given.

--- app/services/calculators/disqualification_calculator.rb
+++ app/services/calculators/disqualification_calculator.rb
@@ -1,12 +1,20 @@
 module Calculators
+  #
+  # If no disqualification end date is given
+  # start_date + 6 months
+  #
+  # This is the default length in this case
+  #
   class DisqualificationCalculator < BaseCalculator
+    REHABILITATION = { months: 6 }.freeze
+
     def expiry_date
       return ResultsVariant::INDEFINITE if indefinite_length?
 
       if disclosure_check.conviction_length?
         conviction_end_date
       else
-        conviction_start_date
+        conviction_start_date.advance(REHABILITATION)
       end
     end
   end
