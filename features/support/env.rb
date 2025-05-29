0f266868d7cc360f48f8c57c089cd480f4e3ed73
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-05-29 12:59:59 +0100

    Install capybara-lockstep gem

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,7 +12,7 @@ Capybara.register_driver(:chrome_headless) do |app|
   args = %w[disable-gpu no-sandbox]
   args << "headless" unless ENV["SHOW_BROWSER"]
 
-  options = Selenium::WebDriver::Chrome::Options.new(args:)
+  options = Selenium::WebDriver::Chrome::Options.new(unhandled_prompt_behavior: "ignore", args:)
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
