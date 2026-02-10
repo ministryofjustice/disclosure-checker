dc00d78f3a1c302760b9f0677bb3850c3c1d0523
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-02-10 14:41:55 +0000

    Add 4 second sleep

--- spec/features/feedback_link_spec.rb
+++ spec/features/feedback_link_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "feedback_link", type: :feature do
   scenario "User clicks the feedback link and sees the feedback page" do
     visit root_path
-    sleep(4)
+    sleep 4
     link = find_link(text: "feedback")
     expect(link[:href]).to include("https://www.smartsurvey.co.uk/s/FNZI5U/")
   end
